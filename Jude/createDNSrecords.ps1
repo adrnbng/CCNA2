f3bd1150ef1ea1a5f6ad42032556e4b6d2e98d33
adrnbng <84377225+adrnbng@users.noreply.github.com>
2024-05-20 16:52:37 +0800

    Add files via upload

---
+++ Jude/createDNSrecords.ps1
@@ -0,0 +1,17 @@
+﻿Install-WindowsFeature  -name dns  -includeManagementTools
+Add-DnsServerPrimaryZone -Name "ngcp12.ph" -ZoneFile "ngcp12.ph.dns"
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name ns -ipv4address 10.12.1.10
+add-DnsServerResourceRecord -zonename ngcp12.ph -Cname -name www -hostname ns.ngcp12.ph
+add-DnsServerResourceRecord -zonename ngcp12.ph -Cname -name imap -hostname ns.ngcp12.ph
+add-DnsServerResourceRecord -zonename ngcp12.ph -Cname -name pop -hostname ns.ngcp12.ph
+add-DnsServerResourceRecord -zonename ngcp12.ph -Cname -name smtp -hostname ns.ngcp12.ph
+#### FOR CISCO DEVICES DNS RECORDS;
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name cb -ipv4address 10.12.1.4
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name ct -ipv4address 10.12.1.2
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name cm -ipv4address 10.12.100.8
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name ed -ipv4address 10.12.12.1
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name p1 -ipv4address 10.12.100.101
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name p2 -ipv4address 10.12.100.102
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name c1 -ipv4address 10.12.50.6
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name c2 -ipv4address 10.12.50.8
+add-DnsServerResourceRecord -zonename ngcp12.ph -A -name ap -ipv4address 10.12.10.3
---
+++ Jude/createweb.ps1
@@ -0,0 +1,2 @@
+﻿Install-WindowsFeature  -name Web-Server  -includeManagementTools
+New-Website -name "ngcp12.ph" -hostheader "www.ngcp12.ph" -physicalpath "d:\webs\datingbiz"
